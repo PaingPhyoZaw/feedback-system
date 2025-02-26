0b8dc2cb0f991d08a10e3949c169539b2440f53c
paingphyozaw <paingphyozaw54@gmail.com>
2025-02-26 23:17:16 +0630

    fix overview page bug

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
   return new PrismaClient({
-    log: ['query'],
+    log: ['error'],
     datasources: {
       db: {
         url: process.env.DATABASE_URL
@@ -19,4 +19,4 @@ const globalForPrisma = global as GlobalWithPrisma
 
 export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+globalForPrisma.prisma = prisma
